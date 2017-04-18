4020d534f6438ec266fb22daec81614b6d80efc4
David Steele <steele@debian.org>
2017-04-17 23:07:05 -0400

    Resolve git to tar

--- src/gitversion/_version.py
+++ src/gitversion/_version.py
@@ -23,8 +23,8 @@ def get_keywords():
     # setup.py/versioneer.py will grep for the variable names, so they must
     # each be defined on a line of their own. _version.py will just call
     # get_keywords().
-    git_refnames = " (HEAD -> develop, tag: 0.9.6)"
-    git_full = "890d9de0bd3a306811ee6a472afa0592c1f48e82"
+    git_refnames = " (tag: 0.9.7)"
+    git_full = "37603c59635f669a5c55dfd10c73bef7109fd657"
     keywords = {"refnames": git_refnames, "full": git_full}
     return keywords
 
